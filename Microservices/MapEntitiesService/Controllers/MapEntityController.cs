8541e32a362c7c400a534dbbbb7f1eb8d11b8441
noatias1234 <noatias1234@gmail.com>
2022-06-23 10:03:28 +0300

    Map entity service initialized Map entity controller implemented

--- Microservices/MapEntitiesService/Controllers/MapEntityController.cs
+++ Microservices/MapEntitiesService/Controllers/MapEntityController.cs
@@ -1,64 +1,20 @@
+using MapEntitiesService.Core.Model;
+using MapEntitiesService.Core.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MapEntitiesService.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class MapEntityController : ControllerBase
+public partial class MapEntityController : ControllerBase
 {
-    private readonly ILogger<MapEntityController> _logger;
+    private readonly IMapEntityService _mapEntityService;
 
-    public record ResultModel (bool Success, string ErrorMessage = "");
-
-    public MapEntityController(ILogger<MapEntityController> logger)
+    public MapEntityController(IMapEntityService mapEntityService)
     {
-        _logger = logger;
+        _mapEntityService = mapEntityService;
     }
 
     [HttpPost]
-    public ResultModel Post(
-        [FromForm]MapEntityDto mapEntityDto, 
-        [FromServices] IPublisher publisher)
-    {
-        _logger.LogInformation("New map entity: Title  - {entityTitle}", mapEntityDto.Title);
-
-        // validate mapEntityDto
-        // if validation is failed, log 
-        if (string.IsNullOrEmpty(mapEntityDto.Title))
-        {
-            return new ResultModel(Success: false, ErrorMessage: "Title not valid");
-        }
-
-        // publish to message broker by topic name NewMapEntity
-        publisher.Publish(topic: "NewMapEntity", mapEntityDto);
-
-        return new ResultModel(Success: true);
-    }
-}
-
-public interface IPublisher
-{
-    void Publish(string topic, MapEntityDto mapEntityDto);
-}
-
-public class Publisher : IPublisher
-{
-    private readonly ILogger<Publisher> _logger;
-
-    public Publisher(ILogger<Publisher> logger)
-    {
-        _logger = logger;
-    }
-
-    public void Publish(string topic, MapEntityDto mapEntityDto)
-    {
-        _logger.LogInformation("Published New Map Entity!!!");
-    }
-}
-
-public class MapEntityDto
-{
-    public string? Title { get; set; }
-    public double? XPosition { get; set; }
-    public double? YPosition { get; set; }
+    public ResultModel Post([FromForm] MapEntityDto mapEntityDto) => _mapEntityService.HandleNewMapEntity(mapEntityDto);
 }
--- Microservices/MapEntitiesService/Program.cs
+++ Microservices/MapEntitiesService/Program.cs
@@ -1,9 +1,17 @@
-using MapEntitiesService.Controllers;
+using MapEntitiesService.Core.Services;
+using MapEntitiesService.Core.Services.Interfaces;
+using MapEntitiesService.Core.Validation;
+using MapEntitiesService.Core.Validation.Interfaces;
+using MapEntitiesService.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//// Add services to the container.
 builder.Services.AddScoped<IPublisher, Publisher>();
+builder.Services.AddScoped<IMapEntityService, MapEntityService>();
+builder.Services.AddScoped<IMapEntityValidator, MapEntityValidator>();
+builder.Services.AddScoped<IMapEntityTitleValidator, MapEntityTitleValidator>();
+builder.Services.AddScoped<ICoordinateValidator, CoordinateValidtor>();
 
 
 builder.Services.AddControllers();
@@ -12,11 +20,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
